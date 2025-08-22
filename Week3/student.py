c08c991452bee986d1d60b414b664605f64575fb
Aditya <95137212+1512aditya@users.noreply.github.com>
2025-08-22 19:17:57 +1200

    Update student.py

--- Week3/student.py
+++ Week3/student.py
@@ -22,5 +22,24 @@ def get_student_by_id(student_id):
     """, (student_id,))
     courses = cursor.fetchall()
 
+def add_student(student_id, student_name):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO STUDENT (student_id, student_name) VALUES (?, ?)",
+        (student_id, student_name)
+    )
+    conn.commit()
+    conn.close()
+    print(f" Student {student_name} added successfully!")
+
+def delete_student(student_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM STUDENT WHERE student_id = ?", (student_id,))
+    conn.commit()
+    conn.close()
+    print(f" Student with ID {student_id} deleted successfully!")
+
     conn.close()
     return student, [c[0] for c in courses]
